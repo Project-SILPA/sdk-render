32a1e2967043cf2c97c0e40e146a650764b0737c
Sujith V <sujithvm24@gmail.com>
2014-08-16 16:40:56 +0530

    move fonts to res and fix bug in renderer

--- src/main/java/org/silpa/render/ScriptRenderer.java
+++ src/main/java/org/silpa/render/ScriptRenderer.java
@@ -1,7 +1,6 @@
 package org.silpa.render;
 
 import android.content.Context;
-import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -18,7 +17,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Stack;
 
 /**
  * Created by sujith on 27/6/14.
@@ -100,6 +98,13 @@ public class ScriptRenderer {
         languageCodes.put("te_IN", 9);
     }
 
+    private static int[] fontsTTF = {R.raw.lohit_bn, R.raw.lohit_gu, R.raw.lohit_hi, R.raw.lohit_kn,
+            R.raw.lohit_ml, R.raw.lohit_or, R.raw.lohit_pa, R.raw.lohit_ta, R.raw.lohit_te,
+            R.raw.roboto_regular};
+
+    private static String[] fontsTTFName = {"lohit_bn.ttf",
+            "lohit_gu.ttf", "lohit_hi.ttf", "lohit_kn.ttf", "lohit_ml.ttf",
+            "lohit_or.ttf", "lohit_pa.ttf", "lohit_ta.ttf", "lohit_te.ttf", "roboto_regular.ttf"};
 
     /**
      * Constructor
@@ -118,7 +123,7 @@ public class ScriptRenderer {
     private void initFontPaths() {
         this.fontPaths = new String[NUM_SUPPORTED_INDIC_LANGUAGES];
         this.fontPaths[0] = this.mContext.getFilesDir() + File.separator + "lohit_bn.ttf";
-        this.fontPaths[1] = this.mContext.getFilesDir() + File.separator + "Roboto-Regular.ttf";
+        this.fontPaths[1] = this.mContext.getFilesDir() + File.separator + "roboto_regular.ttf";
         this.fontPaths[2] = this.mContext.getFilesDir() + File.separator + "lohit_hi.ttf";
         this.fontPaths[3] = this.mContext.getFilesDir() + File.separator + "lohit_gu.ttf";
         this.fontPaths[4] = this.mContext.getFilesDir() + File.separator + "lohit_kn.ttf";
@@ -142,7 +147,12 @@ public class ScriptRenderer {
                                     int bitmapWidth) {
         Bitmap bitmap = Bitmap.createBitmap(bitmapWidth, bitmapHeight, Bitmap.Config.ARGB_8888);
         this.canvas = new Canvas(bitmap);
-        renderIndicText(text, 0, fontSize, fontSize, fontColor);
+
+        int baseLine = fontSize;
+        for (String line : text.split("\n")) {
+            renderIndicText(line, 0, baseLine, fontSize, fontColor);
+            baseLine += fontSize;
+        }
 
         for (int i = 0; i < bitmap.getHeight(); i++) {
             for (int j = 0; j < bitmap.getWidth(); j++) {
@@ -181,6 +191,7 @@ public class ScriptRenderer {
     }
 
     private List<String[]> preprocessString(String text) {
+
         List<String[]> lst = new ArrayList<String[]>();
 
         char[] characters = text.toCharArray();
@@ -190,9 +201,18 @@ public class ScriptRenderer {
         if (strLang == null) {
             strLang = "en_US";
         }
+        if (strLang.equals("ISO15919") || strLang.equals("IPA")) strLang = "en_US";
 
         for (int i = len - 2; i >= 0; i--) {
             String lang = CharacterMap.getLanguage(characters[i]);
+
+            if (lang != null && (lang.equals("ISO15919") || lang.equals("IPA")))
+                lang = "en_US";
+
+            if (lang == null && characters[i] != ' ') {
+                lang = "en_US";
+            }
+
             if (lang == null || lang.equals(strLang)) {
                 str = characters[i] + str;
             } else {
@@ -245,23 +265,16 @@ public class ScriptRenderer {
     private void copyFontFiles() {
 
         String[] files = null;
-        AssetManager assetManager = this.mContext.getAssets();
 
-        try {
-            files = assetManager.list("");
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Failed to get asset file list.", e);
-        }
-        for (String filename : files) {
-            if (!(filename.endsWith(".ttf"))) {
-                continue;
-            }
+        for (int i = 0; i < fontsTTF.length; i++) {
+            int fontsResourceId = fontsTTF[i];
             InputStream in = null;
             OutputStream out = null;
             try {
-                in = assetManager.open(filename);
-                File outFile = new File(this.mContext.getFilesDir() + File.separator + filename);
+                in = this.mContext.getResources().openRawResource(fontsResourceId);
+                File outFile = new File(this.mContext.getFilesDir() + File.separator + fontsTTFName[i]);
                 if (outFile.exists()) {
+                    in.close();
                     continue;
                 }
                 out = new FileOutputStream(outFile);
@@ -275,9 +288,8 @@ public class ScriptRenderer {
                 out.flush();
                 out.close();
                 out = null;
-                System.out.println("Loaded font files");
             } catch (IOException e) {
-                Log.e(LOG_TAG, "Failed to copy asset font file: " + filename, e);
+                Log.e(LOG_TAG, "Failed to copy font file: " + fontsTTFName[i], e);
             }
         }
     }
